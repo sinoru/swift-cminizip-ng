d6b0b99ff157603d297bf3353bcfc431ec2edc32
Jaehong Kang <sinoru@me.com>
2022-05-02 00:30:25 +0900

    Update package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Cminizip-ng",
+    name: "swift-cminizip-ng",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
